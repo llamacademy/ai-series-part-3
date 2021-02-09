b78fde9b02d41e0aa5082f83320bc18e0fe2b33b
Chris Kurhan <chris.kurhan@hotmail.com>
2021-02-09 07:37:21 -0600

    Initial commit

---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent), typeof(AgentLinkMover))]
+public class EnemyMovement : MonoBehaviour
+{
+    public Transform Player;
+    [SerializeField]
+    private Animator Animator;
+    public float UpdateRate = 0.1f;
+    private NavMeshAgent Agent;
+    private AgentLinkMover LinkMover;
+
+    private const string IsWalking = "IsWalking";
+    private const string Jump = "Jump";
+    private const string Landed = "Landed";
+
+    private void Awake()
+    {
+        Agent = GetComponent<NavMeshAgent>();
+        LinkMover = GetComponent<AgentLinkMover>();
+
+        LinkMover.OnLinkEnd += HandleLinkEnd;
+        LinkMover.OnLinkStart += HandleLinkStart;
+    }
+
+    private void Start()
+    {
+        StartCoroutine(FollowTarget());
+    }
+
+    private void HandleLinkStart()
+    {
+        Animator.SetTrigger(Jump);
+    }
+
+    private void HandleLinkEnd()
+    {
+        Animator.SetTrigger(Landed);
+    }
+
+    private void Update()
+    {
+        Animator.SetBool(IsWalking, Agent.velocity.magnitude > 0.01f);
+    }
+
+    private IEnumerator FollowTarget()
+    {
+        WaitForSeconds Wait = new WaitForSeconds(UpdateRate);
+        
+        while(enabled)
+        {
+            Agent.SetDestination(Player.transform.position - (Player.transform.position - transform.position).normalized * 0.5f);
+            yield return Wait;
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,18 @@
+# Navmesh AI Tutorial 3 - How to Animate NavMeshAgents
+The below video is the tutorial that **ENDS** where this project is. If you would like to follow along, check out the [2. Adding Jump to your Navmesh with NavMeshLink](https://github.com/llamacademy/navmesh-ai-tutorial/tree/main/2.%20Adding%20Jump%20to%20your%20Navmesh%20with%20NavMeshLink) folder.
+
+[![Youtube Tutorial](./Video%20Screenshot.png)](https://www.youtube.com/watch?v=wLZPM46zgUo&ref=github)
+
+**IMMEDIATELY AFTER CHECKING THIS OUT** IMPORT THE [UNITY CHAN MODEL](https://assetstore.unity.com/packages/3d/characters/unity-chan-model-18705) FROM THE UNITY ASSET STORE or you will be missing the model! (it's free)
+
+In this project we have:
+1. Simple click to move.
+2. An enemy that follows the player
+3. NavMeshLinks to allow the player and enemy to jump from one platform to another, and on top of some walls.
+4. AgentLinkMover to control how NavMeshAgents will traverse NavMeshLinks
+5. Animated 3D Model based on NavMeshAgent's movement
+
+## Requirements
+* Requires Unity 2019.4 LTS or higher. 
+* Utilizes the [Navmesh Components](https://github.com/Unity-Technologies/NavMeshComponents) from Unity's Github.
+* [Unity-Chan Model](https://assetstore.unity.com/packages/3d/characters/unity-chan-model-18705) from the Unity Asset Store (it's free)
